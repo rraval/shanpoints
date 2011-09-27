6f73d51db8e81434e9814e6181bdccc80a54b74e
Ronuk Raval <ronuk.raval@gmail.com>
2011-09-27 01:37:52 -0400

    Added a simple view listing users with points

--- tracker/views.py
+++ tracker/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from tracker.models import User
+
+def list(request):
+    return render_to_response(
+        'list.html',
+        {'users': User.objects.order_by('-points').all()}
+    )
--- urls.py
+++ urls.py
@@ -4,11 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'shanpoints.views.home', name='home'),
-    # url(r'^shanpoints/', include('shanpoints.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', 'tracker.views.list'),
     url(r'^admin/', include(admin.site.urls)),
 )
